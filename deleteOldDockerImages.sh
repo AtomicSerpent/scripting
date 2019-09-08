b398369f42a6ce316e09621786cb53110d7a44dd
andriyrudenko <andriyrudenko87@gmail.com>
2019-09-08 21:24:32 +0300

    Add functions

--- deleteOldDockerImages.sh
+++ deleteOldDockerImages.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-LOG_DIR='/var/log/' 
-LOG_FILE='del_old_images.log' 
+LOG_DIR='/var/log/'
+LOG_FILE='del_old_images.log'
+PARAM=$1
+DATE_TIME=$(date +%Y-%m-%d-%H-%M)
+IMAGE_LIST= "docker images | awk '{print $3,$4,$5}'"
 
-if [ ! -f $LOG_DIR$LOG_FILE ]; 
-	then touch $LOG_DIR$LOG_FILE; 
+
+function write_log {
+	echo -e "$DATE_TIME\n$1\n" |tee -a $LOG_DIR$LOG_FILE
+}
+
+
+function delete {
+write_log 'Getting list '$IMAGE_LIST
+}
+
+if [ ! -f $LOG_DIR$LOG_FILE ];
+	then
+	echo Creating log file
+	echo..............
+	touch $LOG_DIR$LOG_FILE;
+	echo Done!
 fi
-echo Start delete 
 
+write_log 1234
+
+write_log 123
 #docker images | grep " [months|weeks]* ago" | awk '{print $3}' | xargs docker rmi
