fe49ab2b3ea07fc4d17a778bcb283277ccd88c5b
andriyrudenko <andriyrudenko87@gmail.com>
2019-09-08 20:13:36 +0300

    add log file

--- deleteOldDockerImages.sh
+++ deleteOldDockerImages.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+LOG_DIR='/var/log/' 
+LOG_FILE='del_old_images.log' 
+
+if [ ! -f $LOG_DIR$LOG_FILE ]; 
+	then touch $LOG_DIR$LOG_FILE; 
+fi
+echo Start delete 
+
 #docker images | grep " [months|weeks]* ago" | awk '{print $3}' | xargs docker rmi
