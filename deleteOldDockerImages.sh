321ec7dab3f52b3bc46d3aee61532a5438469d86
AndriyRudenko <andriy.rudenko87@gmail.com>
2019-09-09 15:53:31 +0300

    merge

--- deleteOldDockerImages.sh
+++ deleteOldDockerImages.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 LOG_DIR='/var/log/'
 LOG_FILE='del_old_images.log'
-PARAM=$1
+PARAM_DELETE=$1
 DATE_TIME=$(date +%Y-%m-%d-%H-%M)
 IMAGE_LIST= docker images | awk '{print $2,$3,$4,$5}'
 
 
 function write_log {
-	echo -e "$DATE_TIME\n$1\n" |tee -a $LOG_DIR$LOG_FILE
+	echo -e "\n$DATE_TIME\n$1\n" |tee -a $LOG_DIR$LOG_FILE
 }
 
 
 function delete {
-write_log 'Getting list $IMAGE_LIST'
+	write_log 'Getting_list'
+	docker images | tee -a $LOG_DIR$LOG_FILE
+
 }
+
+
 
 if [ ! -f $LOG_DIR$LOG_FILE ];
 	then
 	echo Creating log file
-	echo..............
+	echo ..............
 	touch $LOG_DIR$LOG_FILE;
 	echo Done!
 fi
-delete
-write_log 1234
 
-write_log 123
+delete
+
+
+
 #docker images | grep " [months|weeks]* ago" | awk '{print $3}' | xargs docker rmi
